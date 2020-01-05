f286e88b4f53cf9a65a523eaa679b22d37cc76a2
Azmain Adel <azmainadel47@gmail.com>
2020-01-05 17:12:54 +0600

    Add solution to "Darts"

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -1,11 +1,28 @@
 class Darts {
+    private static final double OUTER_CIRCLE_RADIUS = 10;
+    private static final double MIDDLE_CIRCLE_RADIUS = 5;
+    private static final double INNER_CIRCLE_RADIUS = 1;
+
+    private double x;
+    private double y;
 
     Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.x = x;
+        this.y = y;
+    }
+
+    double distanceFromCenter(){
+        return Math.sqrt(x * x + y * y);
     }
 
     int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        double distance = distanceFromCenter();
 
+        if(distance > OUTER_CIRCLE_RADIUS) return 0;
+        else if(distance <= OUTER_CIRCLE_RADIUS && distance > MIDDLE_CIRCLE_RADIUS) return 1;
+        else if (distance <= MIDDLE_CIRCLE_RADIUS && distance > INNER_CIRCLE_RADIUS) return 5;
+        else if(distance <= INNER_CIRCLE_RADIUS) return 10;
+
+        return 0;
+    }
 }
